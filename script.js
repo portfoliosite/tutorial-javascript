47282d45fc72e32adfc72ec605fba7b708c03906
DenisK <denisk@devart.com>
2019-08-29 09:58:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// console.log(4/0);
+// console.log('string' * 9);
+
+// let something;
+// console.log(something);
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+
+// let answer = confirm("Are you here");
+// console.log(answer);
+
+// let answer = +prompt("Есть ли вам 18 ?", "Да");
+// console.log(typeof(answer));
+
+// console.log(typeof(null));
+
+// console.log("arr" + " - object");
+
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log("2" === 2);
+
+// let isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || !isClose);
